7fa39ff39ae9d8a047231db90d885a292422bb8c
JoshuaHolloway <jhollow6@asu.edu>
2020-09-01 17:24:58 -0500

    resize event

---
+++ js/event-05_focus.js
@@ -0,0 +1,14 @@
+// https://developer.mozilla.org/en-US/docs/Web/API/Element/focus_event
+const print = (x) => console.log(x);
+const qs = (x) => document.querySelector(x);
+print('hi from event-05_focus.js');
+
+const password = document.querySelector('input[type="password"]');
+
+password.addEventListener('focus', (event) => {
+  event.target.style.background = 'pink';    
+});
+
+password.addEventListener('blur', (event) => {
+  event.target.style.background = '';    
+});
