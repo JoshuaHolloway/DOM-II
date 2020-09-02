58b07d357deeb6530582af623fd2e9aa9c08dda9
JoshuaHolloway <jhollow6@asu.edu>
2020-09-02 09:46:40 -0500

    select event

---
+++ js/event-08.js
@@ -0,0 +1,16 @@
+import {print} from './~.js';
+
+// select event
+// -https://developer.mozilla.org/en-US/docs/Web/API/Element/select_event
+// -The select event fires when some text has been selected.
+print('inside logSelection');
+function logSelection(event) {
+
+
+    const log = document.getElementById('log');
+    const selection = event.target.value.substring(event.target.selectionStart, event.target.selectionEnd);
+    log.textContent = `You selected: ${selection}`;
+}
+
+const input = document.querySelector('#_input_');
+input.addEventListener('select', logSelection);
