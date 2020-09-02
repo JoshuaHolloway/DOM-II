fc9a4c0b5e1bf449209264a83fd3140a6275fb1a
JoshuaHolloway <jhollow6@asu.edu>
2020-09-02 12:29:47 -0500

    event propagation

--- mvp-2/mvp-2.js
+++ mvp-2/mvp-2.js
@@ -1,20 +1,20 @@
 // * [ ] Nest two similar events somewhere in the site and prevent the event propagation properly. Remember not all event types bubble.
 
-// Adapted from here: https://youtu.be/UWCvbwo9IRk
-
 const qs = x => document.querySelector(x);
 const print = x => console.log(x);
 
 const outer_div = qs('#outer-div');
 const inner_div = qs('#inner-div');
-const inner_most = qs('#inner-most');
 
-outer_div.addEventListener('click', () => {
-    print('outer div');
+outer_div.addEventListener('click', (e) => {
+    print('outer div event');
+
 });
-inner_div.addEventListener('click', () => {
-    print('inner div');
-});
-inner_most.addEventListener('click', () => {
-    print('innermost');
+inner_div.addEventListener('click', (e) => {
+    print('inner div event');
+
+    // outer_div.style.backgroundColor = 'red';
+    outer_div.classList.toggle('_purple_');
+
+    e.stopPropagation();
 });
